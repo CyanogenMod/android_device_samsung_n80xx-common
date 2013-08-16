54b455a01e05451e7eb4fdb01fca0e52a44223d2
Espen Fjellvær Olsen <espen@mrfjo.org>
2013-08-16 18:22:48 +0200

    10.2 bringup
    Change-Id: Ic9802ea84b837d885bab18e0b31c120e920c7fe7

--- n80xx-common.mk
+++ n80xx-common.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
     device/samsung/n80xx-common/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc \
     device/samsung/n80xx-common/fstab.smdk4x12:root/fstab.smdk4x12
 
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/n80xx-common/fstab.smdk4x12
+RECOVERY_FSTAB_VERSION := 2
+
 # Audio
 PRODUCT_COPY_FILES += \
     device/samsung/n80xx-common/configs/audio_policy.conf:system/etc/audio_policy.conf
